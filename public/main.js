fd069f27d51fa73962adf78c445597a8fed1a95a
Ryan Lynn <ryanlynn523@gmail.com>
2016-07-26 14:35:29 -0400

    added mouseup and mousedown events

--- public/main.js
+++ public/main.js
@@ -1,11 +1,12 @@
 var socket = io();
 var pictionary = function() {
     var canvas, context;
+    var drawing = true;
 
     var draw = function(position) {
         context.beginPath();
         context.arc(position.x, position.y,
-                         6, 0, 2 * Math.PI);
+            6, 0, 2 * Math.PI);
         context.fill();
     };
 
@@ -14,11 +15,22 @@ var pictionary = function() {
     canvas[0].width = canvas[0].offsetWidth;
     canvas[0].height = canvas[0].offsetHeight;
     canvas.on('mousemove', function(event) {
-        var offset = canvas.offset();
-        var position = {x: event.pageX - offset.left,
-                        y: event.pageY - offset.top};
-        draw(position);
-        socket.emit('draw', position);
+        if (drawing === true) {
+            var offset = canvas.offset();
+            var position = {
+                x: event.pageX - offset.left,
+                y: event.pageY - offset.top
+            };
+            canvas.on('mousedown', function(event) {
+                drawing = true;
+            });
+            canvas.on('mouseup', function(event){
+                drawing = false;
+                return;
+            });
+            draw(position);
+            socket.emit('draw', position);
+        }
     });
     socket.on('draw', draw);
 };
